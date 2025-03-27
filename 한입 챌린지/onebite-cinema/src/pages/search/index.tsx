7244632ed650099e75ce99c4078732d49a1384f8
ekkk1126 <cabi0203@naver.com>
2025-03-27 21:25:15 +0900

    💄 [한입 챌린지 Day4]  한입 씨네마 레이아웃 설정

--- 한입 챌린지/onebite-cinema/src/pages/search/index.tsx
+++ 한입 챌린지/onebite-cinema/src/pages/search/index.tsx
@@ -1,12 +1,18 @@
+import SearchableLayout from '@/components/searchable-layout';
 import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
 
-export default function Home() {
+export default function Page() {
   const router = useRouter();
-  const { q } = router.query;
+  const q = router.query.q;
 
   return (
     <>
-      <h1>검색 결과 : {q}</h1>
+      <h1 style={{ color: 'white' }}>검색 결과 : {q}</h1>
     </>
   );
 }
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
