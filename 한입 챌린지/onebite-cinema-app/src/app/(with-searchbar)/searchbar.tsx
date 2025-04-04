0c5abed2c98010b272ab1bd391699f3a3f8ff18c
ekkk1126 <cabi0203@naver.com>
2025-04-04 14:12:08 +0900

    💄 [한입 챌린지 Day10] 한입 씨네마 서치바 컴포넌트 생성

---
+++ 한입 챌린지/onebite-cinema-app/src/app/(with-searchbar)/searchbar.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+
+export default function Searchbar() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const q = searchParams.get('q');
+
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    setSearch(q || '');
+  }, [q]);
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (!search || q == search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') onSubmit();
+  };
+
+  return (
+    <div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeyDown}
+        placeholder='검색어를 입력하세요...'
+      />
+      <button onClick={onSubmit}>검색</button>
+    </div>
+  );
+}
