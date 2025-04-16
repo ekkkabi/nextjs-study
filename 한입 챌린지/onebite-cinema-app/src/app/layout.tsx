1a9d5bb41fe2d1fd90e650781bc7f5b9f3fe8b28
ekkk1126 <cabi0203@naver.com>
2025-04-16 16:50:54 +0900

    ⚡️ [한입 챌린지 Day18] 한입 씨네마 영화 페이지 인터셉팅 모달 구현

--- 한입 챌린지/onebite-cinema-app/src/app/layout.tsx
+++ 한입 챌린지/onebite-cinema-app/src/app/layout.tsx
@@ -1,19 +1,26 @@
 import Link from 'next/link';
+import { ReactNode } from 'react';
 import './globals.css';
 import style from './layout.module.css';
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: ReactNode;
 }>) {
   return (
     <html lang='ko'>
-      <body className={style.container}>
-        <header className={style.header}>
-          <Link href={'/'}>ONEBITE CINEMA</Link>
-        </header>
-        <main>{children}</main>
+      <body>
+        <div className={style.container}>
+          <header className={style.header}>
+            <Link href={'/'}>ONEBITE CINEMA</Link>
+          </header>
+          <main>{children}</main>
+        </div>
+        {modal}
+        <div id='modal-root' />
       </body>
     </html>
   );
